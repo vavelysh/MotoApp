408787ca69ca99d211591ba0e5a7f4ef2596f249
Adrian Pykacz <46759268+vavelysh@users.noreply.github.com>
2021-01-04 17:45:20 +0100

    create fill-db.sql file to generate data

--- mssql/docker-entrypoint.sh
+++ mssql/docker-entrypoint.sh
@@ -1,3 +1,1 @@
-#!/bin/sh
-
-/usr/src/msql/import-data.sh & /opt/mssql/bin/sqlservr
+/usr/src/app/import-data.sh & /opt/mssql/bin/sqlservr
--- mssql/import-data.sh
+++ mssql/import-data.sh
@@ -1,23 +1,7 @@
 #!/bin/sh
+sleep 90s
+echo "Create schemas..."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure@Password1 -d master -i create-database.sql
+echo "Fill data..."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure@Password1 -d master -i fill-db.sql
 
-# shellcheck disable=SC2039
-# shellcheck disable=SC2034
-echo "SIEMAAA"
-for i in {1..50};
-do
-    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1Secure@Password1 -d master -i setup.sql
-    if [ $? -eq 0 ]
-    then
-        echo "setup.sql completed"
-        break
-    else
-        echo "not ready yet..."
-        sleep 1
-    fi
-done
-/opt/mssql-tools/bin/bcp MotorsportDB.dbo.Nations in "./csv/MotorsportDB.dbo.Nations.csv" -c -t',' -S localhost -U sa -P 1Secure@Password1
-/opt/mssql-tools/bin/bcp MotorsportDB.dbo.Drivers in "./csv/MotorsportDB.dbo.Drivers.csv" -c -t',' -S localhost -U sa -P 1Secure@Password1
-/opt/mssql-tools/bin/bcp MotorsportDB.dbo.Teams in "./csv/MotorsportDB.dbo.Teams.csv" -c -t',' -S localhost -U sa -P 1Secure@Password1
-/opt/mssql-tools/bin/bcp MotorsportDB.dbo.Categories in "./csv/MotorsportDB.dbo.Categories.csv" -c -t',' -S localhost -U sa -P 1Secure@Password1
-/opt/mssql-tools/bin/bcp MotorsportDB.dbo.Seasons in "./csv/MotorsportDB.dbo.Seasons.csv" -c -t',' -S localhost -U sa -P 1Secure@Password1
-/opt/mssql-tools/bin/bcp MotorsportDB.dbo.SeasonCrafts in "./csv/MotorsportDB.dbo.SeasonCrafts.csv" -c -t',' -S localhost -U sa -P 1Secure@Password1
